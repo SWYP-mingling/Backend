d9eb9ab36d5fd6513dd9b8e78821e9ec2273eb92
seyoung <160497402+casylm@users.noreply.github.com>
2026-01-20 23:47:43 +0900

    feat : 모임 생성 API 및 Swagger 문서

---
+++ src/main/java/swyp/mingling/domain/meeting/controller/MeetingController.java
@@ -0,0 +1,36 @@
+package swyp.mingling.domain.meeting.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import swyp.mingling.domain.meeting.dto.CreateMeetingRequest;
+import swyp.mingling.domain.meeting.dto.CreateMeetingResponse;
+import swyp.mingling.global.documentation.MeetingApiDocumentation;
+import swyp.mingling.global.response.ApiResponse;
+
+/**
+ * 모임 관련 API 컨트롤러
+ */
+@Tag(name = "모임 API", description = "모임 생성 및 관리 API")
+@RestController
+@RequestMapping("/meeting")
+public class MeetingController {
+
+    /**
+     * 모임 생성 API
+     *
+     * @param request 모임 생성 요청 DTO
+     * @return 생성된 모임 URL 응답
+     */
+    @MeetingApiDocumentation.CreateMeetingDoc
+    @PostMapping
+    public ApiResponse<CreateMeetingResponse> createMeeting(@Valid @RequestBody CreateMeetingRequest request) {
+        // 목 데이터 응답
+        String mockUrl = "https://mingling.com/meeting/abc123def456";
+        return ApiResponse.success(new CreateMeetingResponse(mockUrl));
+    }
+}
+
