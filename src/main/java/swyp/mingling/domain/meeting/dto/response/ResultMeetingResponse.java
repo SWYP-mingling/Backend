0cbaf006eb2285f5cee7dadcdf62e9201f0ee25a
rkdrudgns0412 <rkdrudgns8233@gmail.com>
2026-01-21 18:15:36 +0900

    # type: 제목
    # 본문 (선택사항)
    # - 무엇을, 왜 변경했는지 설명
    # - 여러 줄 작성 가능
    
    # --- 커밋 타입 ---
    # feat: 새로운 기능 추가
    # fix: 버그 수정
    # docs: 문서 수정
    # style: 디자인 변경
    # refactor: 코드 리팩토링
    # test: 테스트 코드
    # chore: 빌드, 설정 변경
    
    # --- 참고 사항 ---
    # 1. 제목은 명령문으로 작성 (ex: "feat: 로그인 기능 추가")
    # 2. 제목 끝에 마침표(.) 금지
    # 3. 제목과 본문 사이 한 줄 비움
    # 4. # 으로 시작하는 줄은 주석 처리됨

--- src/main/java/swyp/mingling/domain/meeting/dto/response/ResultMeetingResponse.java
+++ src/main/java/swyp/mingling/domain/meeting/dto/response/ResultMeetingResponse.java
@@ -1,15 +1,15 @@
-package swyp.mingling.domain.meeting.dto;
+package swyp.mingling.domain.meeting.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * 모임 생성 응답 DTO
+ * 모임 결과 응답 DTO
  */
 @Getter
 @AllArgsConstructor
-public class CreateMeetingResponse {
+public class ResultMeetingResponse {
 
     @Schema(description = "모임 URL 주소", example = "https://mingling.com/meeting/abc123def456")
     private String meetingUrl;
