a4a86a5f7eecc7054121e979c2c20e5356355c40
rkdrudgns0412 <rkdrudgns8233@gmail.com>
2026-01-21 18:48:32 +0900

    [SW-30] chore: 폴더 구조 변경

--- src/main/java/swyp/mingling/domain/meeting/dto/response/ResultMeetingResponse.java
+++ src/main/java/swyp/mingling/domain/meeting/dto/response/ResultMeetingResponse.java
@@ -1,4 +1,4 @@
-package swyp.mingling.domain.meeting.dto;
+package swyp.mingling.domain.meeting.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
