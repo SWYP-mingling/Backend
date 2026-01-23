989f5d3f1f91dc560e5ec845a4b340cc2555247c
Junsik <junsik213@naver.com>
2026-01-24 03:16:53 +0900

    [SW-27] feat: 출발역 등록 API 스펙 정의

---
+++ src/main/java/swyp/mingling/domain/participant/dto/request/CreateDepartureRequest.java
@@ -0,0 +1,34 @@
+package swyp.mingling.domain.participant.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 출발역 등록 요청 DTO
+ */
+@Getter
+@AllArgsConstructor
+@Schema(description = "출발역 등록 요청 DTO")
+public class CreateDepartureRequest {
+
+    @NotBlank
+    @Schema(description = "참여자 이름", example = "김밍글")
+    private String userName;
+
+    @NotBlank
+    @Schema(description = "참여자 비밀번호", example = "Password!")
+    private String password;
+
+    @NotBlank
+    @Schema(description = "출발역 이름", example = "구로디지털단지역")
+    private String stationName;
+
+    @Schema(description = "위도", example = "37.485266")
+    private Double latitude;
+
+    @Schema(description = "경도", example = "126.901401")
+    private Double longitude;
+
+}
---
+++ src/main/java/swyp/mingling/domain/participant/dto/response/CreateDepartureResponse.java
@@ -0,0 +1,38 @@
+package swyp.mingling.domain.participant.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 출발역 등록 응답 DTO
+ */
+@Getter
+@AllArgsConstructor
+@Schema(description = "출발역 등록 응답 DTO")
+public class CreateDepartureResponse {
+
+    @Schema(description = "참여자 이름", example = "김밍글")
+    private String userName;
+
+    @Schema(description = "출발역 이름", example = "구로디지털단지역")
+    private String stationName;
+
+    @Schema(description = "위도", example = "37.485266")
+    private Double latitude;
+
+    @Schema(description = "경도", example = "126.901401")
+    private Double longitude;
+
+    /**
+     * 정적 팩토리 메서드
+     */
+    public static CreateDepartureResponse of(
+        String userName,
+        String stationName,
+        Double latitude,
+        Double longitude
+    ) {
+        return new CreateDepartureResponse(userName, stationName, latitude, longitude);
+    }
+}
