ee7dd95bb520bd405f81c7338b1177d0b8049fa4
seyoung <160497402+casylm@users.noreply.github.com>
2026-01-25 21:14:01 +0900

    refactor :  모임 생성 응답 객체 수정

--- src/main/java/swyp/mingling/domain/meeting/dto/CreateMeetingResponse.java
+++ src/main/java/swyp/mingling/domain/meeting/dto/CreateMeetingResponse.java
@@ -11,6 +11,4 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CreateMeetingResponse {
 
-    @Schema(description = "모임 URL 주소", example = "https://mingling.com/meeting/abc123def456")
-    private String meetingUrl;
 }
