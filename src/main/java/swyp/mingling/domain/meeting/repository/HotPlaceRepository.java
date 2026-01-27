ffe289a2e0757e23b2697a7c16660cc9ef0ea093
seyoung <160497402+casylm@users.noreply.github.com>
2026-01-27 23:50:05 +0900

    [SW-53] fix : 레포지토리 수정

--- src/main/java/swyp/mingling/domain/meeting/repository/HotPlaceRepository.java
+++ src/main/java/swyp/mingling/domain/meeting/repository/HotPlaceRepository.java
@@ -1,12 +1,13 @@
 package swyp.mingling.domain.meeting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import swyp.mingling.domain.meeting.entity.HotPlace;
-
-import java.util.UUID;
 
 /**
  * 핫플레이스 레포지토리
  */
-public interface HotPlaceRepository extends JpaRepository<HotPlace, UUID> {
+public interface HotPlaceRepository extends JpaRepository<HotPlace, Integer> {
+    @Query(value = "SELECT * FROM hot_place ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    HotPlace findRandomHotPlace();
 }
