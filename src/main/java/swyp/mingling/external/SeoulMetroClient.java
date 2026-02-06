fd89c45f18b86381b9c76ef9d75f5ee954f8c60c
Yongseok-2 <8465867@naver.com>
2026-02-06 11:20:35 +0900

    [SW-78] fix: 지하철 api 시간 수정

--- src/main/java/swyp/mingling/external/SeoulMetroClient.java
+++ src/main/java/swyp/mingling/external/SeoulMetroClient.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import swyp.mingling.external.dto.response.SeoulMetroRouteResponse;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @Slf4j
 @Component
 public class SeoulMetroClient {
@@ -36,13 +33,13 @@ public class SeoulMetroClient {
         String finalStart = start;
         String finalEnd = end;
 
-        String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+//        String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         // 지하철 운행시간 종료 후
-//        String now = "2026-06-06 12:00:00";
+        String now = "2026-06-06 12:00:00";
         return webClient.get()
                 .uri(uriBuilder -> {
                     var uri = uriBuilder
-                            .pathSegment("getShtrmPath", "1", "5", finalStart, finalEnd, now)
+                            .pathSegment("getShtrmPath", "1", "5", finalStart, finalEnd, now, "transfer")
                             .queryParam("schInclYn", "N")
                             .build();
 
